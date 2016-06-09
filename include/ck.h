68d89a0821965e945ce78d7ee984f59a005ff9c4
Leslie Giles <Leslie.Giles@riftio.com>
2016-06-09 16:44:06 -0400

    Adding general include file

---
+++ include/ck.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 RIFT.IO Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __CK_H__
+#define __CK_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <ck_array.h>
+#include <ck_backoff.h>
+#include <ck_barrier.h>
+#include <ck_bitmap.h>
+#include <ck_brlock.h>
+#include <ck_bytelock.h>
+#include <ck_cc.h>
+#include <ck_cohort.h>
+#include <ck_elide.h>
+#include <ck_epoch.h>
+#include <ck_fifo.h>
+#include <ck_hp.h>
+#include <ck_hp_fifo.h>
+#include <ck_hp_stack.h>
+#include <ck_hs.h>
+#include <ck_ht.h>
+#include <ck_limits.h>
+#include <ck_malloc.h>
+#include <ck_pflock.h>
+#include <ck_pr.h>
+#include <ck_queue.h>
+#include <ck_ring.h>
+#include <ck_rwcohort.h>
+#include <ck_rwlock.h>
+#include <ck_sequence.h>
+#include <ck_spinlock.h>
+#include <ck_stack.h>
+#include <ck_stdint.h>
+#ifdef __cplusplus
+}
+#endif
+#endif //__CK_H__
